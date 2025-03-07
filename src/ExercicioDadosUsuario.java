808496ff71d260d0c96e3f0b0a57ba22912b96d2
Lucca Nascimento <luccadev@hotmail.com>
2025-03-07 09:51:51 -0300

    refactor: melhorar o desempenho do codigo

--- src/ExercicioDadosUsuario.java
+++ src/ExercicioDadosUsuario.java
@@ -4,29 +4,36 @@ public class ExercicioDadosUsuario {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nome;
-        int idade, tempoIdade;
-        double altura, alturaCalculada;
+        int idade;
+        double altura;
 
-        //Solicitando o nome do usuário
-        System.out.println("Qual o seu nome?");
-        nome = scanner.nextLine();
 
-        System.out.println("Qual a sua idade?");
+        System.out.print("Qual o seu nome? ");
+        nome = scanner.nextLine().trim();
+
+
+        System.out.print("Qual a sua idade? ");
         idade = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Qual a sua altura");
+        // Solicitação da altura
+        System.out.print("Qual a sua altura (em metros)? ");
         altura = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("\nOlá, " + nome + "!");
 
 
-        System.out.print("Olá, " + nome + "! ");
+        if (idade < 50) {
+            int tempoIdade = 50 - idade;
+            System.out.println("Em " + tempoIdade + " anos, você terá 50 anos.");
+        } else {
+            System.out.println("Você já tem 50 anos ou mais!");
+        }
 
 
-        tempoIdade = 50 - idade;
-        System.out.println("Em " + tempoIdade + " anos você terá 50 anos.");
-
-
-        alturaCalculada = altura * 100;
-        System.out.println("Sua altura em cm é: " + alturaCalculada + " cm");
+        double alturaCalculada = altura * 100;
+        System.out.println("Sua altura em centímetros é: " + alturaCalculada + " cm.");
 
         scanner.close();
     }
